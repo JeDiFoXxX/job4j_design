ba127fedb8c0baddcfddf88a6bca3f8ff7ce4744
JeDiFoXxX <serjik1993991@gmail.com>
2025-02-13 13:50:06 +0300

    Переопределить и equals и hashCode

--- src/main/java/ru/job4j/map/User.java
+++ src/main/java/ru/job4j/map/User.java
@@ -3,6 +3,7 @@ package ru.job4j.map;
 import java.util.Calendar;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Objects;
 
 public class User {
     private String name;
@@ -13,6 +14,23 @@ public class User {
         this.name = name;
         this.children = children;
         this.birthday = birthday;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        if (this == object) {
+            return true;
+        }
+        if (object == null || getClass() != object.getClass()) {
+            return false;
+        }
+        User user = (User) object;
+        return children == user.children && Objects.equals(name, user.name) && Objects.equals(birthday, user.birthday);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, children, birthday);
     }
 
     public static void main(String[] args) {
@@ -30,7 +48,11 @@ public class User {
         map.put(user2, new Object());
         System.out.printf("User%1$d: {hashcode%1$d = %2$d} {hash%1$d = %3$d} {bucket%1$d = %4$d}%n",
                 1, hashCode1, hash1, bucket1);
-        System.out.printf("User%1$d: {hashcode%1$d = %2$d} {hash%1$d = %3$d} {bucket%1$d = %4$d}",
+        System.out.printf("User%1$d: {hashcode%1$d = %2$d} {hash%1$d = %3$d} {bucket%1$d = %4$d}%n",
                 2, hashCode2, hash2, bucket2);
+
+        for (Map.Entry<User, Object> entry : map.entrySet()) {
+            System.out.println(entry.getKey());
+        }
     }
 }
