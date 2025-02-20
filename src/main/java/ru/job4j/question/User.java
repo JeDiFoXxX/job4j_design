b6f18ef83096de6b3beef129ea77fad9539a03b7
JeDiFoXxX <serjik1993991@gmail.com>
2025-02-20 09:49:27 +0300

    Статистика по коллекции

---
+++ src/main/java/ru/job4j/question/User.java
@@ -0,0 +1,46 @@
+package ru.job4j.question;
+
+import java.util.Objects;
+
+public class User {
+    private int id;
+    private String name;
+
+    public User(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        if (this == object) {
+            return true;
+        }
+        if (object == null || getClass() != object.getClass()) {
+            return false;
+        }
+        User user = (User) object;
+        return getId() == user.getId() && Objects.equals(getName(), user.getName());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getId(), getName());
+    }
+}
