72bf331ef97b7a4e65b2023875f1f083bf055ac8
JeDiFoXxX <serjik1993991@gmail.com>
2025-02-23 14:45:07 +0300

    BufferedReader

---
+++ src/main/java/ru/job4j/io/LogFilter.java
@@ -0,0 +1,28 @@
+package ru.job4j.io;
+
+import java.io.*;
+import java.util.*;
+import java.util.stream.Collectors;
+
+public class LogFilter {
+    private final String file;
+
+    public LogFilter(String file) {
+        this.file = file;
+    }
+
+    public List<String> filter() {
+        List<String> list = Collections.emptyList();
+        try (BufferedReader input = new BufferedReader(new FileReader(file))) {
+            list = input.lines().filter(line -> line.contains(" 404 ")).collect(Collectors.toList());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return list;
+    }
+
+    public static void main(String[] args) {
+        LogFilter logFilter = new LogFilter("data/log.txt");
+        logFilter.filter().forEach(System.out::println);
+    }
+}
