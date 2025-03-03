20b26850199a8043581ca60543935b548e8879bf
JeDiFoXxX <serjik1993991@gmail.com>
2025-03-03 14:12:38 +0300

    Валидация параметров запуска

--- src/main/java/ru/job4j/io/Search.java
+++ src/main/java/ru/job4j/io/Search.java
@@ -9,13 +9,29 @@ import java.util.function.Predicate;
 
 public class Search {
     public static void main(String[] args) throws IOException {
-        Path start = Paths.get(".");
-        search(start, path -> path.toFile().getName().endsWith(".js")).forEach(System.out::println);
+        validateArgs(args);
+        Path start = Paths.get(args[0]);
+        search(start, path -> path.toFile().getName().endsWith(args[1])).forEach(System.out::println);
     }
 
     public static List<Path> search(Path root, Predicate<Path> condition) throws IOException {
         SearchFiles searcher = new SearchFiles(condition);
         Files.walkFileTree(root, searcher);
         return searcher.getPaths();
+    }
+
+    private static void validateArgs(String[] args) {
+        if (args.length < 2) {
+            throw new IllegalArgumentException("Нужно передать два аргумента! Используй <DIRECTORY> <EXTENSION>");
+        }
+        Path path = Paths.get(args[0]);
+        String extension = args[1];
+        if (!Files.exists(path) || !Files.isDirectory(path)) {
+            throw new IllegalArgumentException("Указанная директория не существует или не является директорией: "
+                    + args[0]);
+        }
+        if (!extension.startsWith(".")) {
+            throw new IllegalArgumentException("Расширение должно начинаться с точки!");
+        }
     }
 }
