ec514cadd39a1cf234e84533f9ff4e57523a481f
JeDiFoXxX <serjik1993991@gmail.com>
2025-03-10 17:50:54 +0300

    Slf4j - вывод exception

--- src/main/java/ru/job4j/io/EchoServer.java
+++ src/main/java/ru/job4j/io/EchoServer.java
@@ -6,8 +6,13 @@ import java.net.Socket;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class EchoServer {
-    public static void main(String[] args) throws IOException {
+    private static final Logger LOG = LoggerFactory.getLogger(UsageLog4j.class.getName());
+
+    public static void main(String[] args) {
         try (ServerSocket server = new ServerSocket(9000)) {
             while (!server.isClosed()) {
                 Socket socket = server.accept();
@@ -29,6 +34,8 @@ public class EchoServer {
                     output.flush();
                 }
             }
+        } catch (IOException e) {
+            LOG.error("Port 9000 is already in use", e);
         }
     }
 }
